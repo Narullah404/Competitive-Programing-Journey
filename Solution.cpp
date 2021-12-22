aeaaf554fedc476dd5be88b8c02a53a8a0353431
Narullah <narullah1994@gmail.com>
2021-12-22 22:03:46 +0000

    new solution solved

--- Solution.cpp
+++ Solution.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 
 // this file is to try coding problems
+bool isPerfectSquare(double x) {   
+  double sr = sqrt(x); 
+  
+  // If square root is an integer 
+  return ((sr - floor(sr)) == 0); 
+} 
 
 int main() {
 
@@ -10,6 +16,19 @@ int main() {
 	freopen("out.txt", "w", stdout);
 	#endif
 
+	int n;
+	cin >> n;
+
+	for (int i = n - 1; i >= 0; i--){
+		int chk = n - i;
+
+		if ( chk > 3 && (isPerfectSquare(i) || i % 2 == 0) ){
+			if (isPerfectSquare(chk) || chk % 2 == 0) {
+				cout << chk << " " << i << endl;
+				return 0;
+			}
+		}
+	}
 	
 
 	return 0;   
